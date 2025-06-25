6faf7ed005c492095c31524fc2ce4b2e820103b1
Artem Gilmanov <artemapochta@gmail.com>
2025-06-25 10:04:52 +0200

    Added method which collects errors and shows them above the form.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -42,7 +42,17 @@ public class AccountController : Controller
         await _signInManager.SignInAsync(user, isPersistent: false);
         return RedirectToAction("Index", "Home");
       }
+
+      AddErrors(result);
     }
     return View(model);
+  }
+
+  private void AddErrors(IdentityResult result) 
+  {
+    foreach(var error in result.Errors)
+    {
+      ModelState.AddModelError(string.Empty, error.Description);
+    }
   }
 }
